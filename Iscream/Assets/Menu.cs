ad8066131f31caa533836811bd6959b41c2f5f18
Erin's Laptop <nern.strudel@gmail.com>
2018-08-13 15:43:10 -0400

    MENUS YAY

---
+++ Iscream/Assets/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public int levelOneBuildIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelOneBuildIndex);
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
